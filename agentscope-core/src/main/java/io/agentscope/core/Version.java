8213f629ae0393cd4d1fa52592ec97afd3727d20
LearningGp <learninggp4.74@gmail.com>
2026-04-30 10:47:07 +0800

    Prepare 1.0.12 release

--- agentscope-core/src/main/java/io/agentscope/core/Version.java
+++ agentscope-core/src/main/java/io/agentscope/core/Version.java
@@ -24,7 +24,7 @@ package io.agentscope.core;
 public final class Version {
 
     /** AgentScope Java version */
-    public static final String VERSION = "1.0.12-SNAPSHOT";
+    public static final String VERSION = "1.0.12";
 
     private Version() {
         // Utility class - prevent instantiation
